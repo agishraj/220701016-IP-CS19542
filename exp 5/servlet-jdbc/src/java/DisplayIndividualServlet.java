b36a6d5de346a9c27a73b7df8d357b82ab77ac09
agishraj <220701016@rajalakshmi.edu.in>
2024-11-05 12:21:27 +0530

    Update DisplayIndividualServlet.java

--- exp 5/servlet-jdbc/src/java/DisplayIndividualServlet.java
+++ exp 5/servlet-jdbc/src/java/DisplayIndividualServlet.java
@@ -13,11 +13,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
-/**
- *
- * @author CAROLINE
- */
 public class DisplayIndividualServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
